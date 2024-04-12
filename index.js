06edf3557f2b523de26a680a222505526de85a53
dhduarteaOTF <david.duarte@onthefuze.com>
2024-04-12 16:35:08 -0500

    Code to view "video games" and create new video game

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require("dotenv").config()
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,26 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/videogames', async (req, res) => {
+    const contacts = "https://api.hubapi.com/crm/v3/objects/videogames?properties=name,developer,description" ;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -35,37 +23,51 @@ app.get('/contacts', async (req, res) => {
     try {
         const resp = await axios.get(contacts, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        console.error(data);
+        res.render('videogames', { title: 'Videogames | HubSpot APIs', data });      
     } catch (error) {
         console.error(error);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
+app.get('/create', async (req, res) => {
 
     try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+        res.render('create', { title: 'Videogames | HubSpot APIs'});      
     } catch(err) {
         console.error(err);
     }
 
 });
-*/
 
+app.post('/create', async (req, res) => {
+    const create = {
+        "properties":{
+            "name": req.body.name,    
+            "developer": req.body.developer,
+            "description": req.body.description,
+            "rating": req.body.rating,
+            "release_date": req.body.release_date,
+            "genre": req.body.genre,
+            "main_platform": req.body.main_platform
+        }
+    }
+    const createContact = `https://api.hubapi.com/crm/v3/objects/videogames`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {     
+        await axios.post(createContact, create, { headers } );
+        res.redirect('/videogames');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
